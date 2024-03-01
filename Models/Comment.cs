1202639cf60e637a1e7cc25d729634b6bb6190bd
SameerGiri69 <sameergiri153000@gmail.com>
2024-03-01 11:40:55 +0545

    first commit

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace Finshark_API.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
+        public int? StockId { get; set; }
+        // Navigation property - allows us to navigate inside our model
+        public Stock? Stock { get; set; }
+    }
+}
