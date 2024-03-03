9ab6f71926ca71070a3afd41e16030894d917573
SameerGiri69 <sameergiri153000@gmail.com>
2024-03-03 09:08:34 +0545

    Database Calls Segregation

---
+++ Interfaces/IStockInterface.cs
@@ -0,0 +1,15 @@
+﻿using Finshark_API.DTOs.Stock;
+using Finshark_API.Models;
+
+namespace Finshark_API.Interfaces
+{
+    public interface IStockInterface
+    {
+        public Stock GetStockById(int id);
+        public IEnumerable<StockDto> GetAllStocks();
+        public Stock Create(CreateStockDto stock);
+        public Stock UpdateStock(Stock stock, UpdateStockDto updateDto);
+        public bool DeleteStock(int id);
+        public bool Save();
+    }
+}
---
+++ Repository/StockRepository.cs
@@ -0,0 +1,69 @@
+﻿using Finshark_API.Data;
+using Finshark_API.DTOs.Stock;
+using Finshark_API.Interfaces;
+using Finshark_API.Mappers;
+using Finshark_API.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finshark_API.Repository
+{
+    public class StockRepository : IStockInterface
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Stock Create(CreateStockDto stockDto)
+        {
+            var stock = stockDto.ToStockFromCreateDto();
+            _context.Add(stock);
+            Save();
+            return _context.stocks.Find(stock.Id);
+        }
+
+        public bool DeleteStock(int id)
+        {
+            var stock = _context.stocks.Find(id);
+            if (stock == null) return false;
+
+            _context.Remove(stock);
+
+            return Save();
+        }
+
+        public IEnumerable<StockDto> GetAllStocks()
+        {
+            var stocks =  _context.stocks.AsEnumerable().Select(s=>s.ToStockDto());
+            return stocks;
+        }
+
+        public Stock GetStockById(int id)
+        {
+            var stock = _context.stocks.Find(id);
+            return stock;
+        }
+
+        public Stock UpdateStock(Stock myStock, UpdateStockDto updateDto)
+        {
+            
+            myStock.Symbol = updateDto.Symbol;
+            myStock.MarketCap = updateDto.MarketCap;
+            myStock.Purchase = updateDto.Purchase;
+            myStock.LastDiv = updateDto.LastDiv;
+            myStock.CompanyName = updateDto.CompanyName;
+            myStock.Industry = updateDto.Industry;
+
+             Save();
+            return myStock;
+        }
+        public bool Save()
+        {
+            var result =  _context.SaveChanges();
+            if (result < 0) return false;
+            return true;
+        }
+    }
+}
